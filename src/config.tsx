3c4fd872b6bc3dccdda5f830e0e6d692845b73b8
PaudelSworup <sworupkc9@gmail.com>
2023-09-16 22:08:53 +0545

    render

--- src/config.tsx
+++ src/config.tsx
@@ -1,12 +1,12 @@
-export let API: string;
-export let image: string;
-if (
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
-) {
-  API = "http://localhost:8000/api";
-  image = "http://localhost:8000";
-} else {
-  API = "http://192.168.1.73:8000/api";
-  image = "http://192.168.1.73:8000";
-}
+export const API: string = "https://sworup-api.onrender.com/api";
+export let image: string = "https://sworup-api.onrender.com";
+// if (
+//   window.location.hostname === "localhost" ||
+//   window.location.hostname === "127.0.0.1"
+// ) {
+//   API = "https://sworup-api.onrender.com/api";
+//   image = "http://localhost:8000";
+// } else {
+//   API = "https://sworup-api.onrender.com/api";
+//   image = "http://192.168.1.73:8000";
+// }
